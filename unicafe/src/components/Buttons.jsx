44da86220d0378f1b9019edbaf870af3732770da
Smuli99 <hytonensamu99@gmail.com>
2026-09-14 09:00:45 +0300

    task 6.1 done

--- unicafe/src/components/Buttons.jsx
+++ unicafe/src/components/Buttons.jsx
@@ -1,12 +1,18 @@
+import { useFeedbackStore } from "../store";
+
 const Buttons = () => {
+  const {
+    incrementGood, incrementNeutral, incrementBad
+  } = useFeedbackStore(state => state.actions);
+  
   return (
     <div>
       <h2>give feedback</h2>
-      <button>good</button>
-      <button>neutral</button>
-      <button>bad</button>
+      <button onClick={incrementGood}>good</button>
+      <button onClick={incrementNeutral}>neutral</button>
+      <button onClick={incrementBad}>bad</button>
     </div>
-  )
-}
+  );
+};
 
-export default Buttons
+export default Buttons;
