ac71d8f935d1a2e5cfdb3a053c007a2f584288a7
Smuli99 <hytonensamu99@gmail.com>
2026-09-14 12:15:18 +0300

    task 6.6 done

--- anecdotes/src/store.js
+++ anecdotes/src/store.js
@@ -19,6 +19,7 @@ const asObject = anecdote => ({
 
 const useAnecdoteStore = create((set) => ({
   anecdotes: anecdotesAtStart.map(asObject),
+  filter: '',
   actions: {
     vote: (id) => set(
       state => ({
@@ -32,8 +33,16 @@ const useAnecdoteStore = create((set) => ({
         anecdotes: [ ...state.anecdotes, asObject(anecdote) ]
       })
     ),
+    setFilter: (value) => set(() => ({filter: value}))
   },
 }))
 
-export const useAnecdotes = () => useAnecdoteStore((state) => state.anecdotes)
+export const useAnecdotes = () => {
+  const anecdotes = useAnecdoteStore((state) => state.anecdotes);
+  const filter = useAnecdoteStore((state) => state.filter);
+  return anecdotes.filter(anecdote => 
+    anecdote.content.toLowerCase().includes(filter.toLowerCase())
+  );
+};
+
 export const useAnecdoteActions = () => useAnecdoteStore((state) => state.actions);
